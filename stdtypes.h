60b594a3b0eda4db7adb6d7a0d4a8697629df657
mostafammkhalil <mostafa.m.k@ieee.org>
2019-10-26 21:49:28 +0200

    Committing Timer Files

---
+++ stdtypes.h
@@ -0,0 +1,26 @@
+/* Utility File Created By: Mina Raouf
+In this file we define new standard datatypes
+*/
+
+
+#ifndef stdTypes_h
+#define stdTypes_h
+#define NULL ((void *)0)
+typedef enum{NOK,OK}status;
+
+typedef unsigned char         boolean;
+
+typedef unsigned char         uint8;          /*           0 .. 255             */
+typedef signed char           sint8;          /*        -128 .. +127            */
+typedef unsigned short        uint16;         /*           0 .. 65535           */
+typedef signed short          sint16;         /*      -32768 .. +32767          */
+typedef unsigned long         uint32;         /*           0 .. 4294967295      */
+typedef signed long           sint32;         /* -2147483648 .. +2147483647     */
+typedef unsigned long long    uint64;         /*       0..18446744073709551615  */
+typedef signed long long      sint64;
+typedef float                 float32;
+typedef double                float64;
+
+
+
+#endif /* STDTYPES_H_ */
